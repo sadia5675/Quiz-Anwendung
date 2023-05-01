2747b5e5de0389ae4365c62704d7177dd91da48e
aadan001 <anamaria.adanaia@student.hs-rm.de>
2023-05-02 00:58:54 +0200

    PostMapping, HTML fehler ist weg

--- src/main/java/de/hsrm/mi/web/projekt/ui/frage/FrageController.java
+++ src/main/java/de/hsrm/mi/web/projekt/ui/frage/FrageController.java
@@ -1,8 +1,11 @@
 package de.hsrm.mi.web.projekt.ui.frage;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.Model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,4 +22,20 @@ public class FrageController {
         m.addAttribute("fragenr",fragenr);//...und fügt es der Model instanz (Model wird verwendet um Daten an eine View weiterzuleiten)
         return "fragebearbeiten";// gibt die view zurück (fragebearbeiten.html)
     }
+
+    @PostMapping("{fragenr}")
+    public String formular_post(Model m,
+                                @RequestParam("kategorie") String kategorie,
+                                @RequestParam("fragetext") String fragetext,
+                                @RequestParam("richtigeAntwort") String richtigeAntwort,
+                                @RequestParam("punktzahl") int punktzahl
+                                ){
+       //logger zeigt uns die ganzen Sachen die wir eingegeben haben
+        logger.info("Kategorie: {}, Fragetext: {}, Richtige Antwort: {}, Punktzahl: {}",
+                kategorie, fragetext, richtigeAntwort, punktzahl);                            
+
+        return "fragebearbeiten";// gibt die view zurück (fragebearbeiten.html)
+    }
+
+
 }
