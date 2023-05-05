fd1aec317bcaf1630ebe1a6bf79803b37278fbb6
aadan001 <anamaria.adanaia@student.hs-rm.de>
2023-05-05 16:26:18 +0200

    Aufgabe 4 (Session)

--- src/main/java/de/hsrm/mi/web/projekt/ui/frage/FrageController.java
+++ src/main/java/de/hsrm/mi/web/projekt/ui/frage/FrageController.java
@@ -1,9 +1,8 @@
 package de.hsrm.mi.web.projekt.ui.frage;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,45 +13,40 @@ import org.slf4j.LoggerFactory;
 
 @Controller
 @RequestMapping("/frage")//wenn URL-Pfad "/frage" sendet, wird sie von diesen Controller bearbeitet
-@SessionAttributes("fragenformular")//"fragenformular" wird als Attribut in Session-Kontext gespeichert
+@SessionAttributes("frageformular")//"fragenformular" wird als Attribut in Session-Kontext gespeichert
 public class FrageController {
 
-    private static final Logger logger = LoggerFactory.getLogger(FrageController.class); //Ausgabe wie syso
+    //private static final Logger logger = LoggerFactory.getLogger(FrageController.class); //Ausgabe wie syso
 
     //siehe Folie 63... // die Initialisierung
-    @ModelAttribute("fragenformular")//Hier wird ein Attribut "fragenformular" im Model erzeugt und mit dem zurückgegebenen Objekt aus der Methode befüllt
-    public FrageFormular getFrageFormular(){
-        return new FrageFormular("", "", "", "", 0);
-    }
-
-    @GetMapping("{fragenr}") //GET-Anfragen an eine URL die mit /{fragenr} endet die{} bedeutet das es eine Pfadvarible ist
-    public String anzeigen(HttpSession session,@PathVariable String fragenr, Model m, // nimmt fragenr entgegen...
-                        @ModelAttribute("fragenformular") FrageFormular frageFormular ){ 
-
-        frageFormular.setFragenr(fragenr);
-        m.addAttribute("fragenr",fragenr);//...und fügt es der Model instanz (Model wird verwendet um Daten an eine View weiterzuleiten)
+    @ModelAttribute("frageformular")//Hier wird ein Attribut "fragenformular" im Model erzeugt und mit dem zurückgegebenen Objekt aus der Methode befüllt
+    public void initFrageFormular(Model m){
+        FrageFormular formular = new FrageFormular();
        
-        return "fragebearbeiten";// gibt die view zurück (fragebearbeiten.html)
+       /*  formular.setKategorie("");
+        formular.setFragetext("");
+        formular.setRichtigeAntwort("");                  <----- das ganze brauchen wir nicht weil es automatisch schon leer ist. 
+        formular.setPunktzahl(0);
+        */ 
+        m.addAttribute("frageformular", formular);
+        
     }
+
+    @GetMapping("{fragenr}") 
+    public String anzeigen(@PathVariable String fragenr, Model m ){ 
+        m.addAttribute("fragenr",fragenr);
+        return "fragebearbeiten";
+    }
+         
 
     @PostMapping("{fragenr}")
-    public String formular_post(Model m, HttpSession session,
-                                @PathVariable("fragenr") String fragenr, 
-                                @RequestParam("kategorie") String kategorie,
-                                @RequestParam("fragetext") String fragetext,
-                                @RequestParam("richtigeAntwort") String richtigeAntwort,
-                                @RequestParam("punktzahl") int punktzahl,
-                                @ModelAttribute("fragenformular") FrageFormular frageFormular){
+    public String formular_post(Model m, 
+                                @ModelAttribute("frageformular") FrageFormular formular){
         
-        frageFormular.setKategorie(kategorie);
-        frageFormular.setFragetext(fragetext);
-        frageFormular.setRichtigeAntwort(richtigeAntwort);
-        frageFormular.setPunktzahl(punktzahl);
-        session.setAttribute("fragenformular",frageFormular);// speichern vom aktualisierten Formular im Session-Kontext
-        
+                                    
        //logger zeigt uns die ganzen Sachen die wir eingegeben haben
-        logger.info("Kategorie: {}, Fragetext: {}, Richtige Antwort: {}, Punktzahl: {}",
-                kategorie, fragetext, richtigeAntwort, punktzahl);                            
+        //logger.info("Kategorie: {}, Fragetext: {}, Richtige Antwort: {}, Punktzahl: {}",
+                //FrageFormular.getKategorien(), fragetext, richtigeAntwort, punktzahl);                            
 
         return "fragebearbeiten";// gibt die view zurück (fragebearbeiten.html)
     }
--- src/main/java/de/hsrm/mi/web/projekt/ui/frage/FrageFormular.java
+++ src/main/java/de/hsrm/mi/web/projekt/ui/frage/FrageFormular.java
@@ -3,22 +3,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FrageFormular {
-    private String fragenr;
+    
     private String kategorie;
     private String fragetext;
     private String richtigeAntwort;
     private int punktzahl;
 
 
-    //Konstruktor
-    public FrageFormular(String fragenr, String kategorie,String fragetext,String richtigeAntwort,int punktzahl) {
-        this.fragenr= fragenr;
-        this.kategorie = kategorie;
-        this.fragetext = fragetext;
-        this.richtigeAntwort = richtigeAntwort;
-        this.punktzahl = punktzahl;
-
-    }
 
     //ein Getter soll für die Kategorien-Eigenschaft eine Java-Liste mit den Strings "", "Allgemeines", "Zahlen", "Sachen", "Orte", "Ereignisse" zurückliefern
     public static List<String> getKategorien() {
@@ -32,9 +23,6 @@ public class FrageFormular {
         return kategorien;
     }
 
-    public String getFragenr() {
-        return fragenr;
-    }
 
     public String getKategorie() {
         return kategorie;
@@ -52,10 +40,6 @@ public class FrageFormular {
 
     public int getPunktzahl() {
         return punktzahl;
-    }
-
-    public void setFragenr(String fragenr) {
-        this.fragenr = fragenr;
     }
 
     public void setKategorie(String kategorie) {
