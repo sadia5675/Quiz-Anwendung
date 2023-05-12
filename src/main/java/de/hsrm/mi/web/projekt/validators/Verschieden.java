d48688e94d8c9fc5cdfe583c551500688d49479d
Adanaia Ana-Maria <anamaria.adanaia@student.hs-rm.de>
2023-05-12 16:19:22 +0200

    Aufgabe 1,2 feritg, design angepasst

--- src/main/java/de/hsrm/mi/web/projekt/validators/Verschieden.java
+++ src/main/java/de/hsrm/mi/web/projekt/validators/Verschieden.java
@@ -1,8 +1,11 @@
 package de.hsrm.mi.web.projekt.validators;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
-
+@Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy=VerschiedenValidator.class)
 public @interface Verschieden {
 
--- src/main/java/de/hsrm/mi/web/projekt/validators/VerschiedenValidator.java
+++ src/main/java/de/hsrm/mi/web/projekt/validators/VerschiedenValidator.java
@@ -6,11 +6,27 @@ import jakarta.validation.ConstraintValidatorContext;
 public class VerschiedenValidator implements ConstraintValidator<Verschieden, List<String>> {
 
     @Override
-    public boolean isValid(List<String> value, ConstraintValidatorContext context) {
+    public boolean isValid(List<String> liste, ConstraintValidatorContext context) {
+
+        if (liste == null) {
+            return true; 
+        }
+        for (int i = 0; i < liste.size(); i++) {
+            for (int j = i + 1; j < liste.size(); j++) {
+                if (liste.get(i).equalsIgnoreCase(liste.get(j))) {
+                    return false; 
+                }
+            }
+        }
+        return true;
+        
+
+        /* 
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'isValid'");
-    }
+        
+        */
 
-    
+    }
 
 }
