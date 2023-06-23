62b31ac66d2cb02eb776fad0352c2978709e8f1b
Sadia Miah <sadia.miah@student.hs-rm.de>
2023-06-23 16:14:54 +0200

    fehler behoben

--- src/main/java/de/hsrm/mi/web/projekt/api/QuizRestAPIController.java
+++ src/main/java/de/hsrm/mi/web/projekt/api/QuizRestAPIController.java
@@ -1,4 +1,5 @@
 package de.hsrm.mi.web.projekt.api;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -7,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,28 +15,26 @@ import de.hsrm.mi.web.projekt.entities.frage.Frage;
 import de.hsrm.mi.web.projekt.entities.quiz.Quiz;
 import de.hsrm.mi.web.projekt.services.quiz.QuizService;
 
-
-
 @RestController
 public class QuizRestAPIController {
 
     @Autowired
     private QuizService quizService;
 
-
     // Record für Quiz-Liste
-    public record QuizInfoDTO(long id, String name, int nFragen) {}
+    public record QuizInfoDTO(long id, String name, int nFragen) {
+    }
 
     // Record für Frage-Informationen
-    public record FrageDTO(long frageid, String fragetext, List<String> alleantworten, int punkte, String katname) {}
+    public record FrageDTO(long frageid, String fragetext, List<String> alleantworten, int punkte, String katname) {
+    }
 
     // Record für Quiz-DTO
-    public record QuizDTO(long id, String titel, int punktesumme, List<FrageDTO> fragen) {}
-
+    public record QuizDTO(long id, String titel, int punktesumme, List<FrageDTO> fragen) {
+    }
 
     // Objekt Mapper umwandelt ein Java-Objekt in JSON und anders rum
     private ObjectMapper objectMapper = new ObjectMapper();
-
 
     // Link: http://localhost:8080/rest/api/quiz
     @GetMapping("/api/quiz")
@@ -55,7 +53,7 @@ public class QuizRestAPIController {
     }
 
     // Link: http://localhost:8080/rest/quiz/....
-    @GetMapping("/quiz/{id}")
+    @GetMapping("api/quiz/{id}")
     public ResponseEntity<String> getQuizById(@PathVariable("id") long id) throws JsonProcessingException {
         Optional<Quiz> optionalQuiz = quizService.holeQuizMitId(id);
 
@@ -71,9 +69,9 @@ public class QuizRestAPIController {
             for (Frage frage : fragen) {
                 // Füge alle falsche Antworten ein
                 List<String> alleantworten = new ArrayList<>(frage.getFalscheAntworten());
-                //Fügt die richtige Antowrt ein
+                // Fügt die richtige Antowrt ein
                 alleantworten.add(frage.getRichtigeAntwort());
-                //Die Liste wird gemischt, damit man nicht weiß welche Antwort richtig ist
+                // Die Liste wird gemischt, damit man nicht weiß welche Antwort richtig ist
                 Collections.shuffle(alleantworten);
 
                 FrageDTO frageInfo = new FrageDTO(frage.getId(), frage.getFragetext(), alleantworten,
@@ -88,15 +86,15 @@ public class QuizRestAPIController {
             String json = objectMapper.writeValueAsString(quizInfo);
 
             // Es wird ein ResponseEntity-Objekt zurückgegeben, das den JSON-String enthält.
-            return ResponseEntity.ok(json); // Das ok bedeutet -> HTTP-Statuscode 200 (OK). 
-        } else { //notFound() bedeutet -> HTTP-Statuscode 404 (Not Found) 
-            return ResponseEntity.notFound().build(); //Das build()-Aufruf erzeugt ein ResponseEntity-Objekt ohne Inhalt des Antwortkörpers. 
+            return ResponseEntity.ok(json); // Das ok bedeutet -> HTTP-Statuscode 200 (OK).
+        } else { // notFound() bedeutet -> HTTP-Statuscode 404 (Not Found)
+            return ResponseEntity.notFound().build(); // Das build()-Aufruf erzeugt ein ResponseEntity-Objekt ohne
+                                                      // Inhalt des Antwortkörpers.
         }
     }
 
-
-       // Hilfsmethode zur Berechnung der Punktesumme aller Fragen
-       private int berechnePunkteSumme(List<Frage> fragen) {
+    // Hilfsmethode zur Berechnung der Punktesumme aller Fragen
+    private int berechnePunkteSumme(List<Frage> fragen) {
         int summe = 0;
         for (Frage frage : fragen) {
             summe += frage.getPunktzahl();
@@ -104,21 +102,4 @@ public class QuizRestAPIController {
         return summe;
     }
 
-
 }
-
-
-
-
-
-    
-
-
-    
-
-
-
-
-
-
-
